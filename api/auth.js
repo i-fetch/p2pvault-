08229044f7922bd94b4355a30660bf8329f9ac95
Motion <jiben026@gmail.com>
2025-01-05 18:10:10 +0100

    update

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,7 @@
 // src/api/auth.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL =process.env.REACT_APP_API_URL;
 
 // Signup API request
 export const signup = async (userData) => {
