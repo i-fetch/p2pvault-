2af0ced037ea9d668f9ba6716d2753ab2fcf96e6
Motion <jiben026@gmail.com>
2025-01-06 18:16:58 +0100

    update

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -49,6 +49,7 @@ function LoginPage() {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
           },
           body: JSON.stringify(formData),
         });
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -62,6 +62,8 @@ function Signup() {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            "Authorization": `Bearer ${token}`,
+
           },
           body: JSON.stringify(formData),
         });
