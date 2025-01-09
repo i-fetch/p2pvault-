19791958dcff7886e91afa89fd7935d1569fc56a
Motion <benjaminokafor2007@gmail.com>
2025-01-09 10:51:56 +0100

    cryptocard updates

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -180,7 +180,7 @@ const HomePage = () => {
               <img
                 src={crypto.image}
                 alt={crypto.name}
-                className="w-10 h-10 mb-1 "
+                className="w-10 h-10 mb-1 border border-white "
               />
               <h3 className="text-sm font-bold text-white">{crypto.name}</h3>
               <p className="text-gray-400 text-xs">
