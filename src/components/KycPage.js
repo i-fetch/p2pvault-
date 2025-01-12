7a622f7c190d23356f59ab6c400ddda5cfbefe33
Motion <benjaminokafor2007@gmail.com>
2025-01-12 01:58:12 +0100

    ready now

--- src/components/KycPage.js
+++ src/components/KycPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import toast from "react-toastify";
+import { toast } from "react-toastify";
 
 const KycPage = () => {
   const [frontImage, setFrontImage] = useState(null);
