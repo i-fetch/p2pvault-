17ea791eb7559a7a83d8abefb9d3a123e67dcd4c
Motion <benjaminokafor2007@gmail.com>
2025-01-13 17:48:18 +0100

    wahala

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import QRCode  from "qrcode.react";
-
+import { QRCodeCanvas } from "qrcode.react";
 const API_URL = process.env.REACT_APP_API_URL2;
 
 const TransactionForm = () => {
